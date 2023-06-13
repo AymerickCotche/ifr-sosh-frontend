c2be5057e83bd7fa98a42cc10466aa364d4141d1
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-06-13 11:55:43 +0400

    initialize bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container red'>
+        <p>yo</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
